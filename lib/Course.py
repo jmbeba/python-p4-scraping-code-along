45a96b3beffafb91219510f77fdd9666f623d2a0
Johnson Mbeba <john.mbeba@student.moringaschool.com>
2024-01-06 14:23:48 +0300

    code along completed

--- lib/Course.py
+++ lib/Course.py
@@ -1,2 +1,9 @@
 class Course:
-    pass
+    def __init__(self, title='', schedule='', description=''):
+        self.title = title
+        self.schedule = schedule
+        self.description = description
+        
+    def __repr__(self):
+        output = f'\nTitle: {self.title}\nSchedule: {self.schedule}\nDescription: {self.description}\n ------------------'
+        return output
